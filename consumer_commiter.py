46aa04d35d3246c249ad709caff3a8d212188392
Alex Rocha <alex.rochas@yahoo.com.br>
2016-09-22 14:30:21 -0300

    refactor: remove desnecessary file with auto_commit

--- consumer_commiter.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaConsumer
-
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
-                         group_id='events_ent',
-                         enable_auto_commit=True)
-consumer.subscribe(['ec_events_v1'])
-
-for message in consumer:
-    print(message)
