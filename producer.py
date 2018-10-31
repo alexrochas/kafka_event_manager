80fc168870b800e24f4a960fc2d9218535fd85ad
Alex Rocha <alex.rochas@yahoo.com.br>
2018-10-31 00:17:22 -0200

    test: log topic

--- producer.py
+++ producer.py
@@ -4,7 +4,7 @@ import json
 
 
 def send_message(message, kafka_url='localhost:9092', topic='inputTopic'):
-    print("Trying to send message")
+    print('Trying to send message to topic %s' % topic)
     producer = KafkaProducer(bootstrap_servers=[kafka_url])
     json_message = json.dumps(message)
     future = producer.send(topic, json_message.encode())
@@ -14,8 +14,8 @@ def send_message(message, kafka_url='localhost:9092', topic='inputTopic'):
         record_metadata = future.get(timeout=10)
     except KafkaError:
         # Decide what to do if produce request failed...
-        print("Error sending to Topic")
+        print('Error sending to Topic')
         pass
 
-    print("Message sent")
+    print('Message sent')
     return record_metadata
